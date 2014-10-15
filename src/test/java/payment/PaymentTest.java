54b20e49f9caa6d0c761e9c50bf029c7b64799e3
Vincent Montalieu <vincent.montalieu@outlook.com>
2014-10-15 11:24:05 +0200

    PaymentTest

---
+++ src/test/java/payment/PaymentTest.java
@@ -0,0 +1,35 @@
+package payment;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import claim.Claim;
+
+public class PaymentTest {
+
+	Payment p1, p2;
+
+	@Before
+	public void setUp() throws Exception {
+		p1 = new Payment(new Claim(), "12/12/12", PaymentMode.BANKTRANSFER);
+		p2 = new Payment(new Claim(), "11/11/11", PaymentMode.CHECK);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		p1 = null;
+		p2 = null;
+	}
+
+	@Test
+	public void testPayment() {
+		assertEquals(new Claim(), p1.getClaimHandled());
+		assertEquals("12/12/12", p1.getPaymentDate());
+		assertEquals(PaymentMode.BANKTRANSFER, p1.getPayment());
+		assertFalse(p1.equals(p2));
+	}
+
+}
